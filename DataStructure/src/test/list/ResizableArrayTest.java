050872a48eebeaee6174f137ba3702e76e9c1cb7
Soheb Khan <sohebkhan@Sohebs-MacBook-Air.local>
2023-02-19 11:01:42 +0530

    Updated Unit Test

--- DataStructure/src/test/list/ResizableArrayTest.java
+++ DataStructure/src/test/list/ResizableArrayTest.java
@@ -1,27 +1,21 @@
 package test.list;
-
 import main.list.ResizableArray;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class ResizableArrayTest {
     @Test
-    public void testResizableArray() {
+    public void testAdd() {
         ResizableArray list = new ResizableArray();
         list.Add(5);
         assertEquals(5,list.Get(0));
-
         list.Add(10);
         assertEquals(10,list.Get(1));
-
-        list.Add(15);
-        list.Add(16);
-
-        list.Find(15);
-
+    }
+    @Test
+    public void testRemove() {
+        ResizableArray list = new ResizableArray();
+        list.Add(5);
+        assertEquals(5,list.Get(0));
         list.Remove();
-
-
     }
 }

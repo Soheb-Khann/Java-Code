b86420004ae31d41212db385e998632a82e5ed2b
Soheb Khan <sohebkhan@Sohebs-MacBook-Air.local>
2023-02-22 11:03:18 +0530

    Added useful java doc

--- DataStructure/src/main/list/ResizableArray.java
+++ DataStructure/src/main/list/ResizableArray.java
@@ -1,65 +1,104 @@
 package main.list;
 
+/**
+ * Creates a Resizable List of Integer elements like the ArrayList Implementation in Java
+ */
+
 public class ResizableArray {
-    int n;
-    public int i;
-    int list[];
+    private int size;
+    private int indexOfLastElement;
+    private int list[];
+
+    /**
+     * @param size
+     * Takes in the size of the list.
+     * If left blank default size of 50 will be used
+     */
     public ResizableArray(int size){
-        n = size;
-        list = new int[n] ;
-        i = 0;
+        this.size = size;
+        list = new int[this.size] ;
+        indexOfLastElement = 0;
     }
 
     public ResizableArray(){
         list = new int[50] ;
-        i = 0;
+        indexOfLastElement = 0;
     }
 
-    public void Add(int e){
-        list[i] = e;
-        i++;
+    /**
+     * @param num
+     * Takes the number to add at the end of the list
+     */
+    public void add(int num){
+        list[indexOfLastElement] = num;
+        indexOfLastElement++;
     }
 
-    public int Get(int index){
+    /**
+     *@param index
+     * Gets the element at index
+     * @return
+     * Returns the element
+     */
+    public int getElementAtIndex(int index){
         return list[index];
     }
-    public void Set(int index, int e) {
-     list[index] = e;
-    }
-    public int Remove(){
-        --i;
-        System.out.println("Removed : "+list[i]);
-        return list[i];
+
+    /**
+     * Sets element at specified index
+     * @param index
+     * @param num
+     */
+    public void set(int index, int num) {
+     list[index] = num;
     }
 
-    public int Find(int f){
-        for (int j = 0; j < i ; j++) {
-            if(f == list[j]){
-                System.out.println("Element : "+f+" \n Found at index : "+j);
+    /**
+     * Removes last element and returns it
+     */
+    public int remove(){
+        --indexOfLastElement;
+        System.out.println("Removed : "+list[indexOfLastElement]);
+        return list[indexOfLastElement];
+    }
+
+    /**
+     * Finds the specified element in the list and returns its index
+     * @param num
+     */
+    public int find(int num){
+        for (int j = 0; j < indexOfLastElement; j++) {
+            if(num == list[j]){
                 return j;
             }
         }
-        System.out.println("Element Not Found");
         return -1;
     }
 
-    public int lastIndexOf(int e){
+    /**
+     * Returns last index of given element
+     * @param num
+     */
+    public int lastIndexOf(int num){
         int lastIndex = -1;
-        for (int j = 0; j< i ; j++){
-            if (e == list[j]){
+        for (int j = 0; j< indexOfLastElement; j++){
+            if (num == list[j]){
                 lastIndex = j;
             }
         }
         return lastIndex ;
     }
 
-    public int removeIndex(int n){
-        int e = list[n];
-        list[n] = 0;
-        for (int j = n; j < i-1; j++) {
+    /**
+     * Removes element at given index, shifts the elements and returns the removed element
+     * @param index
+     */
+    public int removeIndex(int index){
+        int removedElement = list[index];
+        for (int j = index; j < indexOfLastElement -1; j++) {
         list[j] = list[j+1];
         }
-        --i;
-        return e;
+        --indexOfLastElement;
+        return removedElement;
     }
 }
--- DataStructure/src/test/list/ResizableArrayTest.java
+++ DataStructure/src/test/list/ResizableArrayTest.java
@@ -7,63 +7,62 @@ public class ResizableArrayTest {
     @Test
     public void testAdd() {
         ResizableArray list = new ResizableArray();
-        list.Add(5);
-        assertEquals(5,list.Get(0));
+        list.add(5);
+        assertEquals(5,list.getElementAtIndex(0));
     }
     @Test
     public void testSet() {
         ResizableArray list = new ResizableArray();
-        list.Add(5);
-        list.Add(15);
-        list.Set(0,3);
-        assertEquals(3,list.Get(0));
+        list.add(5);
+        list.add(15);
+        list.set(0,3);
+        assertEquals(3,list.getElementAtIndex(0));
     }
     @Test
     public void testRemove() {
         ResizableArray list = new ResizableArray();
-        list.Add(5);
-        list.Add(15);
-        assertEquals(15,list.Remove());
+        list.add(5);
+        list.add(15);
+        assertEquals(15,list.remove());
     }
     @Test
     public void testFind() {
         ResizableArray list = new ResizableArray();
-        list.Add(5);
-        list.Add(15);
-        list.Add(14);
-        list.Add(17);
-        list.Add(10);
-        assertEquals(3,list.Find(17));
+        list.add(5);
+        list.add(15);
+        list.add(14);
+        list.add(17);
+        list.add(10);
+        assertEquals(3,list.find(17));
     }
     @Test
     public void testLastIndexOf(){
         ResizableArray list = new ResizableArray();
-        list.Add(5);
-        list.Add(4);
-        list.Add(3);
-        list.Add(2);
-        list.Add(5);
-        list.Add(8);
-        list.Add(9);
-        list.Add(5);
-        list.Add(1);
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(5);
+        list.add(8);
+        list.add(9);
+        list.add(5);
+        list.add(1);
         assertEquals(7,list.lastIndexOf(5));
     }
     @Test
     public void testRemoveIndex(){
         ResizableArray list = new ResizableArray();
-        list.Add(5);
-        list.Add(4);
-        list.Add(3);
-        list.Add(2);
-        list.Add(5);
-        list.Add(8);
-        list.Add(9);
-        list.Add(5);
-        list.Add(1);
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(5);
+        list.add(8);
+        list.add(9);
+        list.add(5);
+        list.add(1);
        assertEquals(8,list.removeIndex(5));
-       assertEquals(9,list.Get(5));
-       assertEquals(8,list.i);
+       assertEquals(9,list.getElementAtIndex(5));
 
     }
 }
